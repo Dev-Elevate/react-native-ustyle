e905462b01df4b90838ccf983bbd9a6a1adc6bd5
Rohit Singh <rohit99.ind@gmail.com>
2024-02-23 00:30:26 +0530

    feat: export all rn exports

--- src/index.tsx
+++ src/index.tsx
@@ -26,100 +26,124 @@ import type {
 
 export type { ICustomConfig } from './types';
 
-export function View(_props: ExtendedViewProps) {
+function View(_props: ExtendedViewProps) {
   return null;
 }
 
-export function Text(_props: ExtendedTextProps) {
+function Text(_props: ExtendedTextProps) {
   return null;
 }
 
-export function TextInput(_props: ExtendedTextInputProps) {
+function TextInput(_props: ExtendedTextInputProps) {
   return null;
 }
 
-export function Image(_props: ExtendedImageProps) {
+function Image(_props: ExtendedImageProps) {
   return null;
 }
 
-export function Modal(_props: ExtendedModalProps) {
+function Modal(_props: ExtendedModalProps) {
   return null;
 }
 
-export function Switch(_props: ExtendedSwitchProps) {
+function Switch(_props: ExtendedSwitchProps) {
   return null;
 }
 
-export function FlatList(_props: ExtendedFlatListProps) {
+function FlatList(_props: ExtendedFlatListProps) {
   return null;
 }
 
-export function ScrollView(_props: ExtendedScrollViewProps) {
+function ScrollView(_props: ExtendedScrollViewProps) {
   return null;
 }
 
-export function SectionList(_props: ExtendedSectionListProps) {
+function SectionList(_props: ExtendedSectionListProps) {
   return null;
 }
 
-export function KeyboardAvoidingView(
-  _props: ExtendedKeyboardAvoidingViewProps
-) {
+function KeyboardAvoidingView(_props: ExtendedKeyboardAvoidingViewProps) {
   return null;
 }
 
-export function RefreshControl(_props: ExtendedRefreshControlProps) {
+function RefreshControl(_props: ExtendedRefreshControlProps) {
   return null;
 }
 
-export function ImageBackground(_props: ExtendedImageBackgroundProps) {
+function ImageBackground(_props: ExtendedImageBackgroundProps) {
   return null;
 }
 
-export function Button(_props: ExtendedButtonProps) {
+function Button(_props: ExtendedButtonProps) {
   return null;
 }
 
-export function Pressable(_props: ExtendedPressableProps) {
+function Pressable(_props: ExtendedPressableProps) {
   return null;
 }
 
-export function ModalBase(_props: ExtendedModalBaseProps) {
+function ModalBase(_props: ExtendedModalBaseProps) {
   return null;
 }
 
-export function Accessibility(_props: ExtendedAccessibilityProps) {
+function Accessibility(_props: ExtendedAccessibilityProps) {
   return null;
 }
 
-export function VirtualizedList(_props: ExtendedVirtualizedListProps) {
+function VirtualizedList(_props: ExtendedVirtualizedListProps) {
   return null;
 }
 
-export function TouchableOpacity(_props: ExtendedTouchableOpacityProps) {
+function TouchableOpacity(_props: ExtendedTouchableOpacityProps) {
   return null;
 }
 
-export function ActivityIndicator(_props: ExtendedActivityIndicatorProps) {
+function ActivityIndicator(_props: ExtendedActivityIndicatorProps) {
   return null;
 }
 
-export function TouchableHighlight(_props: ExtendedTouchableHighlightProps) {
+function TouchableHighlight(_props: ExtendedTouchableHighlightProps) {
   return null;
 }
 
-export function TouchableWithoutFeedback(
+function TouchableWithoutFeedback(
   _props: ExtendedTouchableWithoutFeedbackProps
 ) {
   return null;
 }
 
-export function TouchableNativeFeedback(
-  _props: ExtendedTouchableNativeFeedbackProps
-) {
+function TouchableNativeFeedback(_props: ExtendedTouchableNativeFeedbackProps) {
   return null;
 }
 
-export function createConfig<T>(config: T & ConfigBuilder): T {
+function createConfig<T>(config: T & ConfigBuilder): T {
   return config;
 }
+
+export * from 'react-native';
+
+export {
+  View,
+  Text,
+  TextInput,
+  Image,
+  Modal,
+  Switch,
+  FlatList,
+  ScrollView,
+  SectionList,
+  KeyboardAvoidingView,
+  RefreshControl,
+  ImageBackground,
+  Button,
+  Pressable,
+  ModalBase,
+  Accessibility,
+  VirtualizedList,
+  TouchableOpacity,
+  ActivityIndicator,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  TouchableNativeFeedback,
+  createConfig,
+};
