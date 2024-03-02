24e295d1d8ffe12b5756e94c0591d1c17c4b6055
Rohit Singh <rohit99.ind@gmail.com>
2024-03-02 18:05:05 +0530

    feat: token resolution

--- example/rnu.config.ts
+++ example/rnu.config.ts
@@ -1,19 +1,37 @@
 import { createConfig } from 'react-native-ustyle';
 export const CONFIG = createConfig({
-  p: 'padding',
-  m: 'margin',
-  t: 'top',
-  b: 'bottom',
-  l: 'left',
-  r: 'right',
-  h: 'height',
-  w: 'width',
-  bg: 'backgroundColor',
-  c: 'color',
-  mx: 'marginHorizontal',
-  bc: 'borderColor',
-  bw: 'borderWidth',
-  mr: 'marginRight',
+  aliases: {
+    p: 'padding',
+    m: 'margin',
+    t: 'top',
+    b: 'bottom',
+    l: 'left',
+    r: 'right',
+    h: 'height',
+    w: 'width',
+    bg: 'backgroundColor',
+    c: 'color',
+    mx: 'marginHorizontal',
+    bc: 'borderColor',
+    bw: 'borderWidth',
+    mr: 'marginRight',
+  },
+  tokens: {
+    global: {
+      primary: '#0000FF',
+      secondary: '#FFFF00',
+    },
+    space: {
+      'px': 1,
+      '1': 4,
+      '2': 8,
+      '3': 12,
+      '4': 16,
+      '5': 20,
+      '6': 24,
+      '7': 28,
+    },
+  },
 } as const);
 
 type ConfigType = typeof CONFIG;
