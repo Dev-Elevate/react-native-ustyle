dd1a8e7c91e1c81cecd0c67d7072c3b1c8de96f9
Rohit Singh <rohit99.ind@gmail.com>
2024-03-15 00:04:22 +0530

    feat: added support for virtual components with variants support

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native-ustyle';
-
+import { Text, View, VC } from 'react-native-ustyle';
+const { Box, Center, Heading } = VC;
 export default function App() {
   return (
     <View
-      bg={'$primary'}
-      p={'$space$4'}
+      bg={'$secondary'}
+      p={8}
       mx={'$space$5'}
       mr="-$space$4"
       roundedBottom={8}
     >
+      <Box></Box>
+      <Center>
+        <Heading variant="primary">Hello World</Heading>
+      </Center>
       <Text c="$secondary">Open up App.js to start working on your app!</Text>
     </View>
   );
